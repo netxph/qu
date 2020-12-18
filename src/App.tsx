e62a4bb85172e51324711c310f56edb8b3f9731a
VITALIS, Marc <marcabraham.vitalis@amadeus.com>
2020-12-18 20:02:37 +0800

    initial page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <header className="mb-auto">
+            <div>
+                <h3 className="float-md-start mb-0">Qu</h3>
+                <nav className="nav nav-masthead justify-content-center float-md-end">
+                    <a className="nav-link" href="#">New Game</a>
+                </nav>
+            </div>
+        </header>
+    );
 }
 
 export default App;
