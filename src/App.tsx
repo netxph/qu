cb924073645d31f216bf3f060f49db3aeaf45573
VITALIS, Marc <marcabraham.vitalis@amadeus.com>
2020-12-18 23:17:56 +0800

    initial design

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
     return (
-        <header className="mb-auto">
-            <div>
-                <h3 className="float-md-start mb-0">Qu</h3>
-                <nav className="nav nav-masthead justify-content-center float-md-end">
-                    <a className="nav-link" href="#">New Game</a>
-                </nav>
-            </div>
-        </header>
+        <div></div>
     );
 }
 
