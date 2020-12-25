d9e3afc125a4a653b1f8968989e2ac0143521347
VITALIS, Marc <marcabraham.vitalis@amadeus.com>
2020-12-25 23:37:46 +0800

    basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
     return (
+        <Router>
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
                 <div className="container-fluid">
@@ -24,11 +25,17 @@ function App() {
             <main className="container">
                 <div className="bg-light rounded p-5">
                     <h1>Start a New Game</h1>
-                    <p className="lead"> There is no active game currently running. To start a new game, click the new game button below. </p>
-                    <a className="btn btn-lg btn-primary" role="button" href="#">New Game</a>
+                    <p className="lead"> There is no active game currently running. To start a new game, click the new game button below.</p>
+                    <Link className="btn btn-lg btn-primary" to="/game/new" >New Game</Link>
                 </div>
             </main>
         </div>
+            <Switch>
+                <Route path="/game/new">
+                    <h1>New Game</h1>
+                </Route>
+            </Switch>
+        </Router>
     );
 }
 
