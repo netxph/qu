693d78219b479bc1f42e626af0128e199e6b5977
VITALIS, Marc <marcabraham.vitalis@amadeus.com>
2020-12-25 22:31:42 +0800

    template transferred in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,25 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
     return (
-        <div></div>
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
+                <div className="container-fluid">
+                    <span className="navbar-brand mb-0 h1">Qu</span>
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contribute</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <main className="container">
+                <div className="bg-light rounded p-5">
+                    <h1>Start a New Game</h1>
+                    <p className="lead"> There is no active game currently running. To start a new game, click the new game button below. </p>
+                    <a className="btn btn-lg btn-primary" role="button" href="#">New Game</a>
+                </div>
+            </main>
+        </div>
     );
 }
 
