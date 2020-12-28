efd6944412c87f8d206f70421bc5bbc05f7e2cb9
VITALIS, Marc <marcabraham.vitalis@amadeus.com>
2020-12-28 09:49:10 +0800

    compartmentalized

--- src/App.tsx
+++ src/App.tsx
@@ -11,31 +11,45 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
     return (
         <Router>
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
-                <div className="container-fluid">
-                    <span className="navbar-brand mb-0 h1">Qu</span>
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Contribute</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-            <main className="container">
-                <div className="bg-light rounded p-5">
-                    <h1>Start a New Game</h1>
-                    <p className="lead"> There is no active game currently running. To start a new game, click the new game button below.</p>
-                    <Link className="btn btn-lg btn-primary" to="/game/new" >New Game</Link>
-                </div>
-            </main>
-        </div>
-            <Switch>
-                <Route path="/game/new">
-                    <h1>New Game</h1>
-                </Route>
-            </Switch>
+            <div>
+                <AppNav />
+                <main className="container">
+                    <Switch>
+                        <Route exact path="/">
+                            <NoGame />
+                        </Route>
+                        <Route path="/game/new">
+                            <h1>New Game</h1>
+                        </Route>
+                    </Switch>
+                </main>
+            </div>
         </Router>
+    );
+}
+
+function AppNav() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
+            <div className="container-fluid">
+                <span className="navbar-brand mb-0 h1">Qu</span>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link" href="https://github.com/netxph/qu">Contribute</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+function NoGame() {
+    return (
+        <div className="bg-light rounded p-5">
+            <h1>Start a New Game</h1>
+            <p className="lead"> There is no active game currently running. To start a new game, click the new game button below.</p>
+            <Link className="btn btn-lg btn-primary" to="/game/new" >New Game</Link>
+        </div>
     );
 }
 
