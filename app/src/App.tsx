550a5af50b31eec74891b9b02496047378e8d94e
VITALIS, Marc <marcabraham.vitalis@amadeus.com>
2025-02-13 00:21:13 +0800

    working

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,30 +4,60 @@ import './App.css'
 function App() {
   return (
     <main>
-      <nav className="container mx-auto bg-white p-4 shadow-md sticky top-0 z-50" >
-        <div className="flex justify-between items-center">
+      <nav className="qu-nav" >
+        <div className=" flex justify-between items-center">
           <a href="#" className="font-bold text-xl text-gray-800">Qu!</a>
-          <div>
+          <div className="hidden md:flex">
             <ul className="flex space-x-6">
-              <li><a href="#" className="text-gray-600 hover:text-gray-800">Game</a></li>
-              <li><a href="#" className="text-gray-600 hover:text-gray-800">Queue</a></li>
-              <li><a href="#" className="text-gray-600 hover:text-gray-800">Players</a></li>
-              <li><a href="#" className="text-gray-600 hover:text-gray-800">Stats</a></li>
+              <li><a href="#" className="qu-nav-item">Game</a></li>
+              <li><a href="#" className="qu-nav-item">Queue</a></li>
+              <li><a href="#" className="qu-nav-item">Players</a></li>
+              <li><a href="#" className="qu-nav-item">Stats</a></li>
             </ul>
+          </div>
+          <div className="md:hidden">
+            <button className="focus:outline-none">
+              <div className="h-1 w-6 mb-1 bg-gray-600"></div>
+              <div className="h-1 w-6 mb-1 bg-gray-600"></div>
+              <div className="h-1 w-6 bg-gray-600"></div>
+            </button>
           </div>
         </div>
       </nav>
       <section className="container mx-auto p-8">
-        <h1 className="text-3xl font-bold mb-8">Next Queue</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar dignissim orci quis vulputate. Integer sit amet porttitor nisl. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sapien sapien, dignissim porta feugiat a, gravida id mi. Aliquam semper quam quis tortor tristique, ut facilisis ex vehicula. Vestibulum molestie lorem a odio accumsan, in pretium urna ullamcorper. In congue augue vitae sodales vulputate. Phasellus pharetra sapien ipsum, in varius nisl congue vitae. Praesent id viverra lacus, quis vehicula justo. Maecenas id diam ullamcorper ex venenatis tincidunt. Phasellus dictum sapien sit amet placerat pellentesque.</p>
-
-<p>Fusce a vulputate turpis. Pellentesque euismod non leo id bibendum. Vivamus a est id tortor rutrum posuere eu faucibus dui. Maecenas feugiat justo commodo dictum euismod. Vivamus ut commodo ipsum. Duis ut vestibulum leo. Maecenas et nulla ornare, pellentesque sapien sit amet, sagittis turpis.</p>
-
-<p>In hac habitasse platea dictumst. Phasellus maximus pretium laoreet. Proin volutpat nisl non orci luctus semper. Phasellus feugiat mauris ac tellus venenatis vestibulum. Vestibulum scelerisque leo felis, et gravida lacus vestibulum quis. Proin accumsan porta vehicula. Nullam at ante a ante molestie porttitor. Donec nec dui nec velit egestas porttitor. Praesent aliquet neque eget nisi mollis, vitae vehicula dolor feugiat. Proin consequat consectetur cursus. Vestibulum suscipit finibus erat sit amet dignissim. Curabitur pretium justo ut enim ullamcorper convallis. Suspendisse eget purus neque.</p>
-
-<p>Praesent nec posuere augue. Fusce ullamcorper nisl nisi, in feugiat risus bibendum nec. Nunc ut nulla augue. Nulla vestibulum mauris tincidunt, auctor erat eget, molestie velit. Mauris pulvinar, nunc eget aliquet commodo, nibh elit euismod metus, a imperdiet diam lorem vitae massa. Nunc et enim vel lectus laoreet rhoncus sed at massa. Nullam vehicula eros semper metus semper lobortis. Integer dignissim ac mi eu rutrum. Proin sit amet est posuere, lobortis ipsum in, lobortis leo.</p>
-
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at venenatis enim. Ut sit amet velit eu odio facilisis vehicula. Aliquam cursus pulvinar dignissim. Duis porta iaculis ante, non accumsan justo placerat vitae. Praesent in mi mi. Nullam tincidunt rhoncus ligula a malesuada. Nullam ullamcorper tempus est in vehicula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam a neque ac ligula sodales efficitur. Aliquam dictum porta felis, ut consequat ex vestibulum vitae. In sollicitudin lorem odio, sit amet laoreet ante pulvinar ut. Donec ac arcu neque. Nulla sem metus, tincidunt non velit malesuada, viverra laoreet mi.</p>
+        <h1 className="text-3xl font-bold mb-8">Game</h1>
+        <ul className="divide-y divide-gray-100 shadow-md">
+          <li className="flex gap-x-6 py-5">
+            <div className="qu-list-team">
+              <p>Clark Kent</p>
+              <p>Lois Lane</p>
+            </div>
+            <div className="qu-list-team">
+              <p>Bruce Wayne</p>
+              <p>Diana Prince</p>
+            </div>
+          </li>
+          <li className="flex gap-x-6 py-5">
+            <div className="qu-list-team">
+              <p>Bruce Banner</p>
+              <p>Steve Rogers</p>
+            </div>
+            <div className="qu-list-team">
+              <p>Tony Stark</p>
+              <p>Thor Odinson</p>
+            </div>
+          </li>
+          <li className="flex gap-x-4 py-5">
+            <div className="qu-list-team">
+              <p>Wade Wilson</p>
+              <p>Logan Logan</p>
+            </div>
+            <div className="qu-list-team">
+              <p>Scott Summers</p>
+              <p>Charles Xavier</p>
+            </div>
+          </li>
+        </ul>
       </section>
     </main>
   )
