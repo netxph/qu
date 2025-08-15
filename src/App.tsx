2318e5e901868f139c869da2bcc153f9da0cd221
VITALIS, Marc <marcabraham.vitalis@amadeus.com>
2025-08-16 02:04:49 +0800

    add initial tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import './App.css'
 
 function App() {
 
-    return (
-        <>
-            <h1>Qu</h1>
-        </>
-    )
+  return (
+    <>
+      <div className="flex flex-col items-center p-4 min-h-screen">
+        <div className="flex items-center justify-between w-full max-w-lg mb-6">
+          <h1>My Game</h1>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App
