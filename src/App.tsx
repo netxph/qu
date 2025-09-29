957e5417a29938c475acde98d0054319e3b37dac
Marc VITALIS <marc.vitalis@live.com>
2025-09-29 17:54:17 +0800

    initial home design

--- src/App.tsx
+++ src/App.tsx
@@ -4,38 +4,54 @@ function App() {
 
   return (
     <>
-      <header className="font-lato">
-        <h1>Tuesday Group</h1>
-        <p>17 Sept 2024</p>
-        <a href="#">Settings</a>
-        <p>0p 1c : 2v2</p>
+      <header className="font-lato max-w-6xl mx-auto flex justify-between p-6 bg-white">
+        <div>
+          <h1 className="text-4xl font-extrabold">Tuesday Group</h1>
+          <p className="text-gray-400">17 Sept 2024</p>
+        </div>
+        <div className="text-right">
+          <a href="#" className="text-gray-300">Settings</a>
+          <p className="text-gray-400">0p 1c : 2v2</p>
+        </div>
       </header>
-      <main className="font-lato">
-        <div>
-          <h2>Court 1</h2>
-          <div>
-            <p>Player 1</p>
-            <p>Player 2</p>
-          </div>
-          <div>
-            <p>Player 3</p>
-            <p>Player 4</p>
-          </div>
-        </div>
-        <div>
-          <h2>Court 2</h2>
-          <div>
-            <p>Player 5</p>
-            <p>Player 6</p>
-          </div>
-          <div>
-            <p>Player 7</p>
-            <p>Player 8</p>
-          </div>
-        </div>
-        <div>
-          <a href="#">+</a>
-        </div>
+      <main className="font-lato max-w-6xl mt-8 mx-auto p-6">
+        <section className="flex flex-col gap-y-2">
+          <article>
+            <div className="p-4 rounded-lg bg-gray-50 mt-2 shadow-md">
+              <h2 className="text-gray-400 text-2xl font-bold">Court 1</h2>
+              <div className="flex justify-between mt-2">
+                <div>
+                  <p>Player 1</p>
+                  <p>Player 2</p>
+                </div>
+                <div>
+                  <p>Player 3</p>
+                  <p>Player 4</p>
+                </div>
+              </div>
+            </div>
+          </article>
+          <article>
+            <div className="p-4 rounded-lg bg-gray-50 mt-2 shadow-md">
+              <h2 className="text-gray-400 text-2xl font-bold">Court 2</h2>
+              <div className="flex justify-between mt-2">
+                <div>
+                  <p>Player 5</p>
+                  <p>Player 6</p>
+                </div>
+                <div>
+                  <p>Player 7</p>
+                  <p>Player 8</p>
+                </div>
+              </div>
+            </div>
+          </article>
+          <article>
+            <div className="p-6 rounded-lg min-h-12 flex items-center justify-center bg-gray-50 mt-2 shadow-md">
+              <a href="#">+</a>
+            </div>
+          </article>
+        </section>
       </main>
     </>
   )
