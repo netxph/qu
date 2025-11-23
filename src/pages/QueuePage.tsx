da6df3aafc28284e9e642060866a72544dfad98f
Marc VITALIS <marc.vitalis@live.com>
2025-11-24 01:10:09 +0800

    convert data

--- src/pages/QueuePage.tsx
+++ src/pages/QueuePage.tsx
@@ -6,6 +6,66 @@ import Team from '../components/Team'
 import Player from '../components/Player'
 import Menu from '../components/Menu'
 
+const data = [
+  {
+    "court": "Court 1",
+    "teams": [
+      {
+        "id": 1,
+        "players": [
+          { "name": "Alice J." },
+          { "name": "Bob K." }
+        ]
+      },
+      {
+        "id": 2,
+        "players": [
+          { "name": "Charlie L." },
+          { "name": "Dana M." }
+        ]
+      }
+    ]
+  },
+  {
+    "court": "Court 2",
+    "teams": [
+      {
+        "id": 1,
+        "players": [
+          { "name": "Ethan N." },
+          { "name": "Fiona O." }
+        ]
+      },
+      {
+        "id": 2,
+        "players": [
+          { "name": "George P." },
+          { "name": "Hannah Q." }
+        ]
+      }
+    ]
+  },
+  {
+    "court": "Court 3",
+    "teams": [
+      {
+        "id": 1,
+        "players": [
+          { "name": "Isaac R." },
+          { "name": "Jasmine S." }
+        ]
+      },
+      {
+        "id": 2,
+        "players": [
+          { "name": "Kevin T." },
+          { "name": "Lara U." }
+        ]
+      }
+    ]
+  }
+];
+
 export default function QueuePage() {
 
   return (
@@ -15,36 +75,18 @@ export default function QueuePage() {
         <section className="flex flex-col gap-y-2">
 
           <Property />
-          <Queue court="Court 1">
-            <Team id={1}>
-              <Player name="Player1" />
-              <Player name="Player2" />
-            </Team>
-            <Team id={2}>
-              <Player name="Player3" />
-              <Player name="Player4" />
-            </Team>
-          </Queue>
-          <Queue court="Court 2">
-            <Team id={1}>
-              <Player name="Player1" />
-              <Player name="Player2" />
-            </Team>
-            <Team id={2}>
-              <Player name="Player3" />
-              <Player name="Player4" />
-            </Team>
-          </Queue>
-          <Queue court="Court 3">
-            <Team id={1}>
-              <Player name="Player1" />
-              <Player name="Player2" />
-            </Team>
-            <Team id={2}>
-              <Player name="Player3" />
-              <Player name="Player4" />
-            </Team>
-          </Queue>
+
+          {data.map(queue => (
+            <Queue court={queue.court} >
+              {queue.teams.map(team => (
+                <Team id={team.id}>
+                  {team.players.map(player => (
+                    <Player name={player.name} />
+                  ))}
+                </Team>
+              ))}
+            </Queue>
+          ))}
           <EmptyQueue />
           <Menu />
 
