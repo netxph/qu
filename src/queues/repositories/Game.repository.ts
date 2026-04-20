d202f54813bbb5b76aebe9136bcd39abf79167dd
Marc VITALIS <marc.vitalis@live.com>
2026-04-20 18:14:09 +0800

    add db repos

---
+++ src/queues/repositories/Game.repository.ts
@@ -0,0 +1,20 @@
+import { db, AppDb, type GameData } from "../../shared/lib/db"
+import Game from "../models/Game.model";
+
+export class GameRepository {
+    private _db: AppDb;
+
+    constructor(database: AppDb = db) {
+        this._db = database;
+    }
+
+    async getLastGame(): Promise<GameData | undefined> {
+        const data = await this._db.games.toCollection().first();
+
+        if (data) {
+            return new Game(data.id, data.size, data.group);
+        }
+
+        return undefined;
+    }
+}
--- src/shared/lib/db.ts
+++ src/shared/lib/db.ts
@@ -1,1 +1,25 @@
-import Dexie
+import Dexie, { type EntityTable } from 'dexie';
+
+export interface GameData {
+    id: string;
+    size: number;
+    group: string;
+}
+
+export class AppDb extends Dexie {
+    games!: EntityTable<GameData, "id">;
+
+    constructor() {
+        super("Qu");
+
+        this.version(1).stores({
+            games: "id"
+        });
+    }
+}
+
+export const db = new AppDb();
+
+if (process.env.NODE_ENV === "development") {
+    (window as any).db = db;
+}
